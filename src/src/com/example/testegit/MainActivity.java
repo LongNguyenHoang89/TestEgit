6562b8182b896b5074b852fa5ac46d617bf90d56
LongNguyenHoang89 <longnguyenhoang@outlook.com>
2014-06-16 10:44:47 +0300

    Add something to cause conflict

--- src/src/com/example/testegit/MainActivity.java
+++ src/src/com/example/testegit/MainActivity.java
@@ -10,10 +10,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.activity_main);
+	//Hello, test test tests
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean Hello(Menu menu) {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.main, menu);
 	return true;
